76bb61b5938c5c35a9ea5c9c8202f7355e3e9117
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-19 13:19:31 +0100

    uploaded codes

---
+++ 02-data-types.js
@@ -0,0 +1,37 @@
+// Primitive Data Types
+
+const userName = "John Doe";
+const sentence = "I am 25 years old.";
+console.log(typeof sentence);
+
+const count = 60;
+console.log(typeof count);
+
+const isLoggedIn = true;
+console.log(typeof isLoggedIn);
+
+// Null
+let person = null;
+
+// Undefined
+let countValue;
+console.log(countValue);
+
+// Non Primitive Data Types
+
+// Objects
+const user = {
+    firstName: "John",
+    lastName: "Peters",
+    age: 45
+}
+console.log(user);
+
+const newPerson = {name: "John Doe"};
+
+console.log(newPerson);
+
+
+// Arrays
+const stringArray = ["John", "Maxwell"];
+console.log(stringArray);
