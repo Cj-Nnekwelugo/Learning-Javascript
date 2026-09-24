8643e04861f1731ae298c8d8c79f275dbad9147d
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-24 10:29:28 +0100

    uploaded codes

--- arrays.js
+++ arrays.js
@@ -2,3 +2,26 @@
 let furits = ["apple", "orange", "bannan", "grap", "mango", "pear"];
 console.log(furits.length);
 
+
+ const colors = [
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Orange",
+        "Purple",
+        "Pink",
+        "Brown",
+        "Black",
+        "White",
+        "Gray",
+        "Cyan"
+    ];
+
+    function getAnyRandomColor(colorsArray) {
+        const randomColorIndex = Math.floor(Math.random() * colorsArray.length);
+        return colorsArray[randomColorIndex];
+    }
+
+    console.log(getAnyRandomColor(colors));
+
