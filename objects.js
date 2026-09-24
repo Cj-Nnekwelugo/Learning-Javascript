b06adbac9e6e06f6acf020a232fdb1ac08baa3d4
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-24 10:14:19 +0100

    uploaded codes

---
+++ objects.js
@@ -0,0 +1,24 @@
+// object literal
+let studentData = {
+    name: "chinedu",
+    age: 15,
+    class: "Jss3",
+    email: "chinedu@gmail.com",
+};
+
+studentData.name = "obinna";
+console.log(studentData);
+
+// nested objects
+
+let data ={
+    name: "jane",
+    age: 30,
+    location:{
+        town: "manchester city",
+        city: "new-york city",
+        state: "United States",
+    },
+    email: "jane@gmail.com",
+};
+console.log(data);
