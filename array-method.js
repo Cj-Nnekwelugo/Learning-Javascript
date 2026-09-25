77c04f133ac2f87dedce0cb4b55110fa848b722d
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-25 10:39:52 +0100

    uploaded for each method 2nd  exerise

--- array-method.js
+++ array-method.js
@@ -21,3 +21,33 @@ students.forEach((student, index) => {
     console.log(`${index + 1}. ${student.name} - ${student.course} `);
 });
 
+// for each method 2nd exesise
+
+const users = [
+    {
+        name: "obinna okafor",
+        age: 45,
+        occupation: "Doctor"
+    },
+    {
+        name: "okeke okafor",
+        age: 45,
+        occupation: "driver"
+    },
+    {
+        name: "Njeze okafor",
+        age: 65,
+        occupation: "journalist"
+    },
+    {
+        name: "obinna okafor",
+        age: 35,
+        occupation: "farmer"
+    }
+];
+
+users.forEach((users, index) => {
+    console.log(
+        `${index + 1}. My name is ${users.name}, I am ${users.age} years old. I am a ${users.occupation}.`
+    );
+});
