a5bbcf9949d5c8c95e2006b0f4a4a885928d1596
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-25 18:44:00 +0100

    uploaded class-constructor.js exerise

--- class-constructor.js
+++ class-constructor.js
@@ -1,14 +1,16 @@
 class Person {
-    constructor(name, age) {
+    constructor(name, age, occupation) {
         this.name = name;
         this.age = age;
+        this.occupation = occupation;
+
     }
 
     introduce() {
-        console.log(`My name is ${this.name}`);
+        console.log(`My name is ${this.name}, i am ${this.age} years old, i am a ${this.occupation}.`);
     }
 }
 
-const person1 = new Person("John Doe", 33);
+const person1 = new Person("John Doe", 33, "Farmer");
 console.log(person1);
 person1.introduce();
