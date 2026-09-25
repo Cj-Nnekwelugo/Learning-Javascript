bd48ed84d7397f7ee2691cbb2eb788235027cfce
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-25 17:33:04 +0100

    uploaded callback exerise

---
+++ callback.js
@@ -0,0 +1,13 @@
+function runSomething(callback) {
+    callback();
+}
+
+runSomething(function() {
+    console.log("Hello!");
+});
+
+// this is the callback function() {
+ //   console.log("Hello!");
+// }
+
+
